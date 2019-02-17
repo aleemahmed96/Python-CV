2e37a4db26029b71b4ec1fa29585d88fb11c828f
danidavid <33230485+danidavid@users.noreply.github.com>
2019-02-17 20:25:29 +0500

    Update lanes.py

--- lanes.py
+++ lanes.py
@@ -8,6 +8,7 @@ def make_coordinates(image, line_parameters):
     y1 = image.shape[0]
     y2 = int(y1 * (3/5))
     x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
     return np.array([x1, y1, x2, y2])
 
 
